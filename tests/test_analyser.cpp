db1f6747a70eac841675306e9c673767f1b55568
Mogg <m1664907189@gmail.com>
2019-11-27 13:57:56 +0800

    first commit

--- tests/test_analyser.cpp
+++ tests/test_analyser.cpp
@@ -3,7 +3,34 @@
 #include "instruction/instruction.h"
 #include "tokenizer/tokenizer.h"
 #include "analyser/analyser.h"
+#include <sstream>
 
 /*
 	不要忘记写测试用例喔。
 */
+TEST_CASE("nmd") {
+
+    std::string input =
+        "begin     \n"
+        "var a = 1;\n"
+        "var b;    \n"
+        "var c;    \n"
+        "var d;    \n"
+        "var e;    \n"
+        "b = a;    \n"
+        "e = b;    \n"
+        "d = e;    \n"
+        "c = a;    \n"
+        "print(c); \n"
+        "end       \n";
+
+    std::stringstream ss;
+    ss.str(input);
+    miniplc0::Tokenizer tkz(ss);
+    auto result = tkz.AllTokens();
+    miniplc0::Analyser a(result.first);
+    a.Analyse();
+    if (result.second.has_value()) {
+        FAIL();
+    }
+}
--- tests/test_tokenizer.cpp
+++ tests/test_tokenizer.cpp
@@ -7,21 +7,29 @@
 
 // 下面是示例如何书写测试用例
 TEST_CASE("Test hello world.") {
-	/*
-	std::string input = 
-		"begin\n"
-		"	var a = 1;\n"
-		"	const b = 1\n"
-		"	print(a+b);\n"
-		"end\n";
-	std::stringstream ss;
-	ss.str(input);
-	miniplc0::Tokenizer tkz(ss);
-	std::vector<miniplc0::Token> output = {};
-	auto result = tkz.AllTokens();
-	if (result.second.has_value()) {
-		FAIL();
-	}
-	REQUIRE( (result.first == output) );
-	*/
+
+    std::string input =
+        "begin\n"
+        "	var a = 1;\n";
+    /*"	const b = 1\n"
+    "	print(a+b);\n"
+    "end\n";*/
+    std::stringstream ss;
+    ss.str(input);
+    miniplc0::Tokenizer tkz(ss);
+    std::vector<miniplc0::Token> output =
+    {
+        miniplc0::Token(miniplc0::TokenType::BEGIN,std::string("begin"),0,0,0,5),
+        miniplc0::Token(miniplc0::TokenType::VAR,std::string("var"),1,1,1,4),
+        miniplc0::Token(miniplc0::TokenType::IDENTIFIER,std::string("a"),1,5,1,6),
+        miniplc0::Token(miniplc0::TokenType::EQUAL_SIGN,'=',1,7,1,8),
+        miniplc0::Token(miniplc0::TokenType::UNSIGNED_INTEGER,1,1,9,1,10),
+        miniplc0::Token(miniplc0::TokenType::SEMICOLON,';',1,10,1,11),
+    };
+    std::cout << output[0].GetValueString();
+    auto result = tkz.AllTokens();
+    if (result.second.has_value()) {
+        FAIL();
+    }
+    REQUIRE((result.first == output));
 }
